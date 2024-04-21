7cb9c2c871be6ee277599a455f313ea88d79de83
G-Just <gjustas00@gmail.com>
2024-04-21 14:53:46 +0300

    General development

--- src/views/Prices.vue
+++ src/views/Prices.vue
@@ -1,82 +1,43 @@
 <template>
-  <section class="container col-span-5 p-6 mx-auto font-mono">
-    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-      <div class="w-full overflow-x-auto">
-        <table class="w-full">
-          <thead>
-            <tr
-              class="font-semibold tracking-wide text-left text-gray-900 uppercase bg-gray-100 border-b border-gray-600 text-md"
-            >
-              <th class="px-4 py-3">Currency</th>
-              <th class="px-4 py-3">Buy</th>
-              <th class="px-4 py-3">Sell</th>
-            </tr>
-          </thead>
-          <tbody class="bg-white">
-            <tr class="text-gray-700">
-              <td class="px-4 py-3 border">
-                <div class="flex items-center text-sm">
-                  <div class="relative w-8 h-8 mr-3 rounded-full md:block">
-                    <img
-                      class="object-cover w-full h-full rounded-full"
-                      src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ3VycmVuY3lEdXBsaWNhdGUiLCJ3IjoxLCJoIjoxLCJzY2FsZSI6MX1d/7111e35254/CurrencyDuplicate.png"
-                      alt=""
-                      loading="lazy"
-                    />
-                    <div
-                      class="absolute inset-0 rounded-full shadow-inner"
-                      aria-hidden="true"
-                    ></div>
-                  </div>
-                  <div>
-                    <p class="font-semibold text-black">Mirror of Kalandra</p>
-                  </div>
-                </div>
-              </td>
-              <td class="px-4 py-3 font-semibold border text-ms">
-                <div class="flex items-center gap-3">
-                  <div
-                    class="px-2 flex items-center justify-center gap-0.5 rounded-lg outline-neutral-400 outline"
-                  >
-                    <div class="relative w-8 h-8 rounded-full md:block">
-                      <img
-                        class="object-cover w-full h-full rounded-full"
-                        src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ3VycmVuY3lEdXBsaWNhdGUiLCJ3IjoxLCJoIjoxLCJzY2FsZSI6MX1d/7111e35254/CurrencyDuplicate.png"
-                        alt=""
-                        loading="lazy"
-                      />
-                    </div>
-                    <p>1</p>
-                  </div>
-                  <p class="pb-1 text-2xl">&rarr;</p>
-                  <div
-                    class="px-2 flex items-center justify-center gap-0.5 rounded-lg outline-neutral-400 outline"
-                  >
-                    <p>50.5K</p>
-                    <div class="relative w-8 h-8 rounded-full md:block">
-                      <img
-                        class="object-cover w-full h-full rounded-full"
-                        src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ3VycmVuY3lSZXJvbGxSYXJlIiwidyI6MSwiaCI6MSwic2NhbGUiOjF9XQ/d119a0d734/CurrencyRerollRare.png"
-                        alt=""
-                        loading="lazy"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td class="px-4 py-3 text-xs border">
-                <span
-                  class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"
-                >
-                  Acceptable
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </section>
+  <div class="col-span-5 p-10">
+    <table class="w-full font-mono text-white bg-slate-700">
+      <thead class="p-4 border-y border-blue-gray-100 bg-blue-gray-50/50">
+        <td>Currency</td>
+        <td>Buy</td>
+        <td>Sell</td>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="p-4 border-b border-blue-gray-50">
+            <div class="flex items-center gap-2 px-2 rounded-lg outline outline-gray-400">
+              <img
+                src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ3VycmVuY3lEdXBsaWNhdGUiLCJ3IjoxLCJoIjoxLCJzY2FsZSI6MX1d/7111e35254/CurrencyDuplicate.png"
+                alt=""
+              />
+              <p class="font-bold">Mirror of Kalandra</p>
+            </div>
+          </td>
+          <td class="flex gap-4 p-4 border-b border-blue-gray-50">
+            <div class="flex items-center gap-2 px-2 rounded-lg outline outline-gray-400">
+              <img
+                src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ3VycmVuY3lEdXBsaWNhdGUiLCJ3IjoxLCJoIjoxLCJzY2FsZSI6MX1d/7111e35254/CurrencyDuplicate.png"
+                alt=""
+              />
+              <p class="font-bold">1</p>
+            </div>
+            <p class="pb-1 text-4xl">&rarr;</p>
+            <div class="flex items-center gap-2 px-2 rounded-lg outline outline-gray-400">
+              <p class="font-bold">50.6K</p>
+              <img
+                src="https://web.poecdn.com/gen/image/WzI1LDE0LHsiZiI6IjJESXRlbXMvQ3VycmVuY3kvQ3VycmVuY3lSZXJvbGxSYXJlIiwidyI6MSwiaCI6MSwic2NhbGUiOjF9XQ/d119a0d734/CurrencyRerollRare.png"
+                alt=""
+              />
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
